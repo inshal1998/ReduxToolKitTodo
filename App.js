0ad51b20263dd12faf6d9334e63e871f3d81a429
insal1998 <insalansari80@gmail.com>
2022-01-12 15:51:14 +0530

    project reorganized

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Profile } from './app/screen';
+import { AuthScreen } from './app/screen';
 import { store } from "./app/redux/store";
 import { Provider } from "react-redux";
 
 const App = () => {
   return (
     <Provider store={store}>
-        <Profile/>
+        <AuthScreen/>
     </Provider>
 
   )
